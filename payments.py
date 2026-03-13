3fb4749b881d7e820318c43d6e6c4b597c5e9f07
kirgizthebest-droid <kirgizthebest@gmail.com>
2026-03-14 00:16:30 +0300

    Update payments.py

--- payments.py
+++ payments.py
@@ -5,19 +5,25 @@ PACKAGES = {
     "pack_3": {
         "title": "3 песни",
         "credits": 3,
-        "price": 100
+        "price_stars": 100,
+        "price_rub": 199,
+        "price_usdt": 2
     },
 
     "pack_10": {
         "title": "10 песен",
         "credits": 10,
-        "price": 250
+        "price_stars": 250,
+        "price_rub": 399,
+        "price_usdt": 4
     },
 
     "pack_50": {
         "title": "50 песен",
         "credits": 50,
-        "price": 900
+        "price_stars": 900,
+        "price_rub": 999,
+        "price_usdt": 10
     }
 
 }
@@ -27,6 +33,6 @@ def create_invoice(package):
 
     pack = PACKAGES[package]
 
-    prices = [LabeledPrice(pack["title"], pack["price"])]
+    prices = [LabeledPrice(pack["title"], pack["price_stars"])]
 
     return prices
