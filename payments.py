3efb3eb5d56d7bdb63125fd180451705c04d0091
kirgizthebest-droid <kirgizthebest@gmail.com>
2026-03-14 00:38:26 +0300

    Update payments.py

--- payments.py
+++ payments.py
@@ -1,38 +1,18 @@
-from telegram import LabeledPrice
-
 PACKAGES = {
 
-    "pack_3": {
-        "title": "3 песни",
-        "credits": 3,
-        "price_stars": 100,
-        "price_rub": 199,
-        "price_usdt": 2
-    },
+"pack3":{
+"credits":3,
+"price":199
+},
 
-    "pack_10": {
-        "title": "10 песен",
-        "credits": 10,
-        "price_stars": 250,
-        "price_rub": 399,
-        "price_usdt": 4
-    },
+"pack10":{
+"credits":10,
+"price":399
+},
 
-    "pack_50": {
-        "title": "50 песен",
-        "credits": 50,
-        "price_stars": 900,
-        "price_rub": 999,
-        "price_usdt": 10
-    }
-
+"pack50":{
+"credits":50,
+"price":999
 }
 
-
-def create_invoice(package):
-
-    pack = PACKAGES[package]
-
-    prices = [LabeledPrice(pack["title"], pack["price_stars"])]
-
-    return prices
+}
